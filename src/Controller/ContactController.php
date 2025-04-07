7536f82d74d1390e462d84801701bfba33f6af2e
Ahmad THIAM <ahmad.thiam@BTSSIO.DEDYN.IO>
2025-04-07 12:07:03 +0200

    07/04/2025

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -4,18 +4,26 @@ namespace App\Controller;
 
 use App\Entity\Contact;
 use App\Repository\ContactRepository;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ContactController extends AbstractController
 {
     #[Route('/contactscontroller', name: 'app_contacts_controller', methods:'GET')]
-    public function listeContact(ContactRepository $repo): Response
+    public function listeContact(ContactRepository $repo)
     { 
         $lesContacts=$repo->findAll();
+        ($lesContacts);
         return $this->render('contact_controller/listeContacts.html.twig',[
             'lesContacts' => $lesContacts
         ]);
     }
+
+   #[Route('/contactcontroller/{id}', name: 'app_ficheContact_controller', methods:'GET')]
+    public function ficheContact(leContact $leContact )
+    { 
+        return $this->render('contact_controller/ficheContact.html.twig',[
+            'leContact' => $leContact
+        ]);
+    } 
 }
---
+++ src/Entity/Categorie.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CategorieRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CategorieRepository::class)]
+class Categorie
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $libelle = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $image = null;
+
+    #[ORM\Column(length: 255, nullable:true)]
+    private ?text $description = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getLibelle(): ?string
+    {
+        return $this->libelle;
+    }
+
+    public function setLibelle(string $libelle): static
+    {
+        $this->libelle = $libelle;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(string $image): static
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+}
